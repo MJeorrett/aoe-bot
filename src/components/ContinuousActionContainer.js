90d372277dc80c878703ceb472f6cfd0754c677b
Matthew Jeorrett <Matthew@wallet.services>
2020-04-23 21:37:39 +0100

    refactor unecessary step

--- src/components/ContinuousActionContainer.js
+++ src/components/ContinuousActionContainer.js
@@ -1,24 +1,13 @@
 import { connect } from 'react-redux';
 
-import { actions, selectors } from '../store';
+import { actions } from '../store';
 import ContinuousAction from './ContinuousAction';
-
-const mapStateToProps = () => {
-  const selectTime = selectors.actions.makeSelectTimeById();
-
-  return (state, { action, action: { id } }) => ({
-    action: {
-      ...action,
-      time: selectTime(state, id),
-    }
-  });
-};
 
 const mapDispatchToProps = (dispatch, { action: { id } }) => ({
   setTime: (newTime) => dispatch(actions.actions.setTime(id, newTime))
 });
 
 export default connect(
-  mapStateToProps,
+  null,
   mapDispatchToProps,
 )(ContinuousAction);
