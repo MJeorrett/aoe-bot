b47917fe870c909cd193ca66ab426568a7ffb61c
Matthew Jeorrett <Matthew@wallet.services>
2020-04-24 14:22:38 +0100

    Fix bug

--- src/components/ResourcesContainer.js
+++ src/components/ResourcesContainer.js
@@ -4,10 +4,15 @@ import { selectors } from '../store';
 
 import Resources from './Resources';
 
-const mapStateToProps = state => ({
-  resourcesByTime: selectors.meta.resourcesByTime(state),
-  time: selectors.timeline.previewTime(state) || selectors.timeline.time(state),
-});
+const mapStateToProps = state => {
+  const previewTime = selectors.timeline.previewTime(state);
+  const time = selectors.timeline.time(state);
+
+  return {
+    resourcesByTime: selectors.meta.resourcesByTime(state),
+    time: previewTime < 0 ? time : previewTime,
+  };
+};
 
 export default connect(
   mapStateToProps,
