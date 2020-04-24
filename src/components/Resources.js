b43c8908efe9fe04bb6b6b82336c1bd1defa7b5c
Matthew Jeorrett <Matthew@wallet.services>
2020-04-24 11:53:21 +0100

    refactor

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -1,23 +1,29 @@
 import React from 'react';
 
+const renderResource = (value, title, round) => (
+  <span>{title}: {round ? Math.floor(value) : value}; </span>
+);
+
 const Resources = ({
   resourcesByTime,
   time,
 }) => {
-  if (!time) {
+  if (time < 0) {
     return (
       <h4>Food: ---; Wood: ---; Stone: ---; Gold: ---</h4>
     );
   }
 
-  const resources = resourcesByTime[time - 1];
-  
+  const resources = resourcesByTime[time];
+
+  const round = false;
+
   return (
     <h4>
-      <span>Food: {Math.floor(resources.food)}; </span>
-      <span>Wood: {Math.floor(resources.wood)}; </span>
-      <span>Stone: {Math.floor(resources.stone)}; </span>
-      <span>Gold: {Math.floor(resources.gold)}; </span>
+      {renderResource(resources.food, "Food", round)}
+      {renderResource(resources.wood, "Wood", round)}
+      {renderResource(resources.stone, "Stone", round)}
+      {renderResource(resources.gold, "Gold", round)}
     </h4>
   );
 };
