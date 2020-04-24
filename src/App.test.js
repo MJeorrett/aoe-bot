68de523141896afa3a1931e69f7195cf91f034c4
Matthew Jeorrett <Matthew@wallet.services>
2020-04-24 10:38:23 +0100

    Fix app test

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Age of Empires Build Order Tool/i);
   expect(linkElement).toBeInTheDocument();
 });
